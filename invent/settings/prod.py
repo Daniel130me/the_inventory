e0e033c1602d9d9f4fdcd17d496d5ceb2d65be44
Oluwagbenga Kosoko <69328757+Daniel130me@users.noreply.github.com>
2023-06-29 00:07:10 +0100

    Update prod.py

--- invent/settings/prod.py
+++ invent/settings/prod.py
@@ -7,6 +7,6 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','54.184.142.184']
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
