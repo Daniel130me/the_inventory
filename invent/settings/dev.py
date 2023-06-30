6cdcf881390db740cb605bd7ba31c4a1251e5c30
Oluwagbenga Kosoko <69328757+Daniel130me@users.noreply.github.com>
2023-06-30 15:02:58 +0100

    Update dev.py

--- invent/settings/dev.py
+++ invent/settings/dev.py
@@ -6,6 +6,6 @@ SECRET_KEY = 'django-insecure-ga_3i@(j$d7@^(f2r&wq2nt(p_*g!!2^zkd870uj*(c!m2c=6w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['54.208.36.165']
 
 
